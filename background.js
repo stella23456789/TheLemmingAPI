589a990ab372590c660555aca87a795c3f94536a
stella <184557442+stella23456789@users.noreply.github.com>
2025-11-15 00:19:16 -0500

    Update background.js

--- background.js
+++ background.js
@@ -1,27 +1,4 @@
 window.addEventListener('DOMContentLoaded', () => {
-    const renders = [
-        'lemming/render1.png',
-        'lemming/render2.png',
-        'lemming/render3.png',
-        'lemming/render4.png',
-        'lemming/render5.png',
-        'lemming/render6.png',
-        'lemming/render7.png',
-        'lemming/render8.png',
-        'lemming/render9.png',
-        'lemming/render10.png',
-        'lemming/render11.png',
-        'lemming/render12.png',
-        'lemming/render13.png',
-        'lemming/render14.png',
-        'lemming/render15.png',
-        'lemming/render16.png',
-        'lemming/render17.png',
-        'lemming/render18.png',
-        'lemming/render19.png',
-        'lemming/render20.png'
-    ];
-
-    const pick = renders[Math.floor(Math.random() * renders.length)];
-    document.body.style.backgroundImage = `url('${pick}')`;
+    const renders = Array.from({ length: 20 }, (_, i) => `lemming/render${i + 1}.png`);
+    document.body.style.backgroundImage = `url('${renders[(Math.random() * 20) | 0]}')`;
 });
