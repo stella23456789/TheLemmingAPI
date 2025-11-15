0367487b9ed416d74e25e86c12c3836d4eaf16d7
stella <184557442+stella23456789@users.noreply.github.com>
2025-11-15 00:09:55 -0500

    Enter this-- Lemming

--- background.js
+++ background.js
@@ -1,5 +1,10 @@
 window.addEventListener('DOMContentLoaded', () => {
-    const renders = ['render1.png', 'render2.png', 'render3.png'];
+    const renders = [
+        'lemming/render1.png',
+        'lemming/render2.png',
+        'lemming/render3.png'
+    ];
+
     const pick = renders[Math.floor(Math.random() * renders.length)];
     document.body.style.backgroundImage = `url('${pick}')`;
 });
